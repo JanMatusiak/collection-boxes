f431bf03fc481aa2b8e634fb6fc006df4c1ccec9
JanMatusiak <jan.matusiak.2006@gmail.com>
2025-06-13 10:39:00 +0200

    Initial import of Spring Boot project

---
+++ src/main/java/com/sii/collection_boxes/CollectionBoxesApplication.java
@@ -0,0 +1,13 @@
+package com.sii.collection_boxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionBoxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionBoxesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sii/collection_boxes/CollectionBoxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sii.collection_boxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionBoxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
